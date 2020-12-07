5980063b5a03d0d052c85d62b662a1853ace84ba
faustapra <73335878+faustapra@users.noreply.github.com>
2020-12-07 23:51:52 +0200

    v1

---
+++ Sources/Utils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+struct Player;
+struct Plate;
+
+namespace nmUtils
+{
+    bool IsBetween(float val, float rangeB, float rangeE);
+    bool IsOnPlate(const Player &player, const Plate &plate);
+}
